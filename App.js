fca6fcd67494014f324615b405822bd0b4251426
Shah hussain <shahhussaint6@gmail.com>
2024-04-28 19:39:39 +0500

    Add TextInput

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput value='Shah hussain'/>
       <Text>Welcome to ci/cd couurse</Text>
       <StatusBar style="auto" />
     </View>
