e404a7bd45cce87323039a684381dea4f2e47304
Shah hussain <shahhussaint6@gmail.com>
2024-04-28 20:10:45 +0500

    Feature-1 ready

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,TextInput } from 'react-native';
+import Greeting from './src/components/Greeting';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TextInput value='Shah hussain'/>
-      <Text>Welcome to ci/cd couurse</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Greeting/>
   );
 }
 
---
+++ src/components/Greeting.js
@@ -0,0 +1,52 @@
+import { View, Text,TextInput, SafeAreaView, StyleSheet, StatusBar } from 'react-native'
+import React, { useState } from 'react'
+
+
+const Greeting = () => {
+    const [message,setMessage]=useState("")
+
+    
+    const handlePress=()=>{
+        setMessage(message)
+
+    }
+
+  return (
+
+    <SafeAreaView style={styles.container}>
+
+
+ 
+    <View>
+     <TextInput
+     style={styles.textInput}
+     placeholder='Write your message...'
+    
+     onChangeText={setMessage}
+     />
+
+     <Text>hello,{message}</Text>
+    </View>
+
+    </SafeAreaView>
+  )
+}
+
+
+const styles=StyleSheet.create({
+    container:{
+    
+        marginTop:StatusBar.currentHeight,
+        padding:10,
+    
+    },
+    textInput:{
+        borderColor: 'blue', 
+        padding:5,
+        borderWidth: 1,
+        textAlign:'left',
+        marginBottom:20
+    }
+})
+
+export default Greeting
